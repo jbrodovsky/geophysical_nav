3971b9b94cb73f0ee43ddcf5d4e4a830501798b9
James Brodovsky <james.brodovsky@gmail.com>
2024-07-12 11:51:34 -0400

    fixed conda/bash bug

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Note: Due to some sort of issue with environment variables, the script must be run with the source command:
+# source setup_env.sh
+# --- NOT ----
+# ./setup_env.sh OR bash setup_env.sh
+
 # Set up the environment for the project by checking to see if miniconda is installed and if so activate the base environment and create the environment for the project
 # If miniconda is not installed, install it and create the environment for the project
 
@@ -11,6 +16,7 @@ then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     bash Miniconda3-latest-Linux-x86_64.sh -b
     rm Miniconda3-latest-Linux-x86_64.sh
+    conda init --all
     source ~/.bashrc
 else
     echo "Miniconda is already installed."
@@ -19,7 +25,6 @@ fi
 # Create the environment for the project
 echo "Creating the environment for the project..."
 conda env create --file environment.yml --name geonav
-conda init bash
 conda activate geonav
 echo "Installing addtional PIP-only packages..."
 python -m pip install -r requirements.txt
