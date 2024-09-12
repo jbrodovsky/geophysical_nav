19ab4d496327d3da639d5653fadec5d430690afd
jbrodovsky <james.brodovsky@gmail.com>
2024-09-12 15:53:03 -0400

    Remove unused attic module and its associated functions

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 """Setup script for the geonav package."""
 
-#from mypyc.build import mypycify
+# from mypyc.build import mypycify
 from setuptools import setup
 
 setup(
     name="geonav",
     packages=["data_management"],
-#    ext_modules=mypycify(paths=[
-#        #"./src/data_managment/dbmgr.py",
-#        #"./src/data_managment/m77t.py",
-#        "./src/ctest/fib.py"]),
+    #    ext_modules=mypycify(paths=[
+    #        #"./src/data_managment/dbmgr.py",
+    #        #"./src/data_managment/m77t.py",
+    #        "./src/ctest/fib.py"]),
 )
